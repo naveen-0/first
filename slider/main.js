d25e181523d365066542891dae84a69cfe422778
naveen-0 <33219916+naveen-0@users.noreply.github.com>
2019-01-25 14:46:32 +0530

    add first project

---
+++ slider/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".carousel-control-next").click(function(){
+        $("#ram img:first-child").css({
+        width: "100%",
+        height: "100%",
+        opacity: "1"
+        });
+    });
+     $("#ram img:first-child").css({
+        width: "100%",
+        height: "100%",
+        opacity: "1"
+        });
+});
